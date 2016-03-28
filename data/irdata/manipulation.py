dc3edb74797350c47986e7d649ea290f7c1b3edb
Aaron Olkin <aaronolkin@gmail.com>
2016-03-28 16:52:05 -0400

    IR data files

---
+++ data/irdata/manipulation.py
@@ -0,0 +1,28 @@
+
+
+def getdata(fn):
+    fd = open(fn)
+    lines = fd.readlines()
+    fd.close()
+    return list([i.strip().split(",") for i in lines])
+
+def writedata(fn, data):
+    fd = open(fn,"w")
+    fd.write("\n".join([",".join(i) for i in data]))
+    fd.close()
+
+FILES = list(["{}-s.csv".format(i) for i in range(1,7)]) + list(["{}-m.csv".format(i) for i in range(1,3)])
+
+
+def setaverages(fn, newfn):
+    data = {}
+    for cm, v in getdata(fn):
+        if cm in data:
+            data[cm] = str((float(data[cm]) + float(v)) / 2)
+        else:
+            data[cm] = v
+    writedata(newfn, sorted(data.items(),key=lambda x:float(x[0])))
+
+
+Medium: 70, 1000
+Short: 30, 50
