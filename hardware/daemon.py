f98419c7813f680c7805705eb01b56b638a9ad74
Aaron Olkin <aaronolkin@gmail.com>
2016-03-31 12:13:14 -0400

    Added __main__ for hardware daemon

--- hardware/daemon.py
+++ hardware/daemon.py
@@ -1,6 +1,13 @@
 
 import atexit, time, sys, os
 
+from .color import ColorSensor
+from .distance import InterpolatedDistance
+from .leds import LEDController
+from .melexis import Melexis
+from .pwm import MotorDriver
+from .sound import SoundDetect
+
 BASE_PATH = "data/irdata/"
 
 files1 = ("1-s.avg.csv","2-s.avg.csv","3-s.avg.csv","4-s.avg.csv")
@@ -8,7 +15,6 @@ files2 = ("5-s.avg.csv","6-s.avg.csv","1-m.avg.csv","2-m.avg.csv")
 
 COMPONENTS = (
     ColorSensor(),
-    Melexis(),
     InterpolatedDistance.from_files(0x48, *files1, base_path=BASE_PATH),
     InterpolatedDistance.from_files(0x49, *files2, base_path=BASE_PATH),
     LEDController(),
@@ -17,16 +23,16 @@ COMPONENTS = (
     SoundDetect()
 )
 
-for i in COMPONENTS:
-    i.init()
-
 @atexit.register
 def cleaner():
     for i in COMPONENTS:
         i.cleanup()
 
 for i in COMPONENTS:
-    i.start()
+    i.init()
 
-while True:
-    time.sleep(1)
+def main():
+    for i in COMPONENTS:
+        i.start()
+    while True:
+        time.sleep(1)
